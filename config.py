6319fb4ce0ead293c35864f516b210f6afce7b48
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-16 17:08:14 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 from os import environ
 from Script import script
 
-id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
@@ -11,68 +10,74 @@ def is_enabled(value, default):
         return False
     else:
         return default
-      
-# Bot Information
-API_ID = int(environ.get("API_ID", "15479023"))
-API_HASH = environ.get("API_HASH", "f8f6cf547822449c29fc60dae3b31dd4")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7845374433:AAGsstCb801Ry-pQSNF-gNbdARqZqKH913I")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1512442581').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "Testaf1bot") # without @
-PORT = environ.get("PORT", "8080")
+try:
+    id_pattern = re.compile(r'^.\d+$')
+    
+    # Bot Information
+    API_ID = int(environ.get("API_ID", "15479023"))
+    API_HASH = environ.get("API_HASH", "f8f6cf547822449c29fc60dae3b31dd4")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7845374433:AAGsstCb801Ry-pQSNF-gNbdARqZqKH913I")
 
-# Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+    PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+    ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1512442581').split()]
+    BOT_USERNAME = environ.get("BOT_USERNAME", "Testaf1bot") # without @
+    PORT = environ.get("PORT", "8080")
 
-# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://testclone:test123@testclone.gnnmw7g.mongodb.net/?retryWrites=true&w=majority&appName=testclone")
-CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
+    # Clone Info :-
+    CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
-# Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://test:test123@test.eccvyc9.mongodb.net/?retryWrites=true&w=majority&appName=test")
-DB_NAME = environ.get("DB_NAME", "techvjbotz")
+    # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+    CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://testclone:test123@testclone.gnnmw7g.mongodb.net/?retryWrites=true&w=majority&appName=testclone")
+    CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
-# Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+    # Database Information
+    DB_URI = environ.get("DB_URI", "mongodb+srv://test:test123@test.eccvyc9.mongodb.net/?retryWrites=true&w=majority&appName=test")
+    DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
-# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+    # Auto Delete Information
+    AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
 
-# Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002855763957"))
+    # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+    AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
+    AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
-# File Caption Information
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+    # Channel Information
+    LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002855763957"))
 
-# Enable - True or Disable - False
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+    # File Caption Information
+    CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+    BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
-# Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+    # Enable - True or Disable - False
+    PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+    # Verify Info :-
+    VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
-# Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+    # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+    SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
+    SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
+    VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
-# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+    # Website Info:
+    WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
 
-# File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+    # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+    WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
 
-# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-MULTI_CLIENT = False
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+    # File Stream Config
+    STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+
+    # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+    MULTI_CLIENT = False
+    SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+    else:
+        ON_HEROKU = False
+    URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+except Exception as e:
+    print("⚠️ Error loading config.py:", e)
+    traceback.print_exc()
