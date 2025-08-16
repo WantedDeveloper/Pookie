61c7f679cc84692ae56bb6659b640832f71e36b6
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-16 17:11:36 +0530

    Add files via upload

--- plugins/dbusers.py
+++ plugins/dbusers.py
@@ -7,6 +7,8 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.bot = self.db.clone_bots
+        self.settings = self.db.bot_settings
 
     def new_user(self, id, name):
         return dict(
@@ -31,5 +33,36 @@ class Database:
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
+
+    async def add_clone_bot(self, bot_id, user_id, first_name, username, bot_token):
+        settings = {
+            'is_bot': True,
+            'bot_id': bot_id,
+            'user_id': user_id,
+            'name': first_name,
+            'username': username,
+            'token': bot_token,
+            'wlc': script.START_TXT,
+            'pics': None
+        }
+        await self.bot.insert_one(settings)
+
+    async def is_clone_exist(self, user_id):
+        clone = await self.bot.find_one({'user_id': int(user_id)})
+        return bool(clone)
+
+    async def get_clone(self, user_id):
+        clones = await self.bot.find({"user_id": int(user_id)}).to_list(length=100)
+        return clones
+    
+    async def update_clone(self, bot_id, user_data):
+        await self.bot.update_one({'bot_id': int(bot_id)}, {'$set': user_data}, upsert=True)
+
+    async def delete_clone(self, bot_id):
+        await self.bot.delete_one({'bot_id': int(bot_id)})
+        await self.settings.delete_many({'bot_id': int(bot_id)})
+
+    async def get_db_size(self):
+        return (await self.db.command("dbstats"))['dataSize']
 
 db = Database(DB_URI, DB_NAME)
