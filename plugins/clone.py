3e5b69f99c75a746a57acd34a901e73c53d5c48c
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-17 15:06:20 +0530

    Add files via upload

--- plugins/clone.py
+++ plugins/clone.py
@@ -5,13 +5,12 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
 from config import API_ID, API_HASH, DB_URI, DB_NAME, CLONE_MODE
-from plugins.dbusers import db
 
 mongo_client = MongoClient(DB_URI)
 mongo_db = mongo_client["cloned_vjbotz"]
 
 async def restart_bots():
-    bots = list(db.bots.find())
+    bots = list(mongo_db.bots.find())
     for bot in bots:
         bot_token = bot['token']
         try:
