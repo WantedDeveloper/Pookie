5ca5482fafd5d0368020230b1998bae9180291b6
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-18 08:46:30 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import re
-import os
-from os import environ
+import re, os
 from Script import script
 
 def is_enabled(value, default):
@@ -18,73 +16,63 @@ def is_enabled(value, default):
 try:
     id_pattern = re.compile(r'^.\d+$')
 
-    PORT = environ.get("PORT", "8080")
+    PORT = os.environ.get("PORT", "8080")
 
     # Bot Information
-    API_ID = int(environ.get("API_ID", "15479023"))
-    API_HASH = environ.get("API_HASH", "f8f6cf547822449c29fc60dae3b31dd4")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7845374433:AAGsstCb801Ry-pQSNF-gNbdARqZqKH913I")
-    BOT_USERNAME = environ.get("BOT_USERNAME", "Testaf1bot") # without @
+    API_ID = int(os.environ.get("API_ID", "15479023"))
+    API_HASH = os.environ.get("API_HASH", "f8f6cf547822449c29fc60dae3b31dd4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7845374433:AAGsstCb801Ry-pQSNF-gNbdARqZqKH913I")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Testaf1bot") # without @
 
     # Database Information
-    DB_URI = environ.get("DB_URI", "mongodb+srv://test:test123@test.eccvyc9.mongodb.net/?retryWrites=true&w=majority&appName=test")
-    DB_NAME = environ.get("DB_NAME", "techvjbotz")
+    DB_URI = os.environ.get("DB_URI", "mongodb+srv://test:test123@test.eccvyc9.mongodb.net/?retryWrites=true&w=majority&appName=test")
+    DB_NAME = os.environ.get("DB_NAME", "techvjbotz")
 
     # Clone Database Information
-    CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://testclone:test123@testclone.gnnmw7g.mongodb.net/?retryWrites=true&w=majority&appName=testclone")
-    CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
+    CLONE_DB_URI = os.environ.get("CLONE_DB_URI", "mongodb+srv://testclone:test123@testclone.gnnmw7g.mongodb.net/?retryWrites=true&w=majority&appName=testclone")
+    CDB_NAME = os.environ.get("CDB_NAME", "clonetechvj")
 
     # Moderator Information
-    OWNERS = [int(owner) if id_pattern.search(owner) else owner for owner in environ.get('OWNERS', '1512442581').split()]
-    ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1512442581').split()]
+    OWNERS = [int(owner) if id_pattern.search(owner) else owner for owner in os.environ.get('OWNERS', '1512442581').split()]
+    ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMINS', '1512442581').split()]
 
     # Channel Information
-    LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002855763957"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002855763957"))
 
     # Start Information
-    WLC = environ.get("WLC", None)
-    PICS = environ.get("PICS", None)
+    WLC = os.environ.get("WLC", None)
+    PICS = os.environ.get("PICS", None)
 
     # Auto Delete Information
-    AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+    AUTO_DELETE_MODE = bool(os.environ.get('AUTO_DELETE_MODE', True)) # Set True or False
 
     # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-    AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-    AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+    AUTO_DELETE = int(os.environ.get("AUTO_DELETE", "30")) # Time in Minutes
+    AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
     # File Caption Information
-    CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-    BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+    CUSTOM_FILE_CAPTION = os.environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+    BATCH_FILE_CAPTION = os.environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
     # Enable - True or Disable - False
-    PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+    PUBLIC_FILE_STORE = is_enabled((os.environ.get('PUBLIC_FILE_STORE', "True")), True)
 
     # Verify Info :-
-    VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+    VERIFY_MODE = bool(os.environ.get('VERIFY_MODE', False)) # Set True or False
 
     # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-    SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-    SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
-    VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-
-    # Website Info:
-    WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
-
-    # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-    WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+    SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "") # shortlink domain without https://
+    SHORTLINK_API = os.environ.get("SHORTLINK_API", "") # shortlink api
+    VERIFY_TUTORIAL = os.environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
     # File Stream Config
-    STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+    STREAM_MODE = bool(os.environ.get('STREAM_MODE', True)) # Set True or False
 
     # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
     MULTI_CLIENT = False
-    SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    if 'DYNO' in environ:
-        ON_HEROKU = True
-    else:
-        ON_HEROKU = False
-    URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+    SLEEP_THRESHOLD = int(os.environ.get('SLEEP_THRESHOLD', '60'))
+    PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    
 except Exception as e:
     print("⚠️ Error loading config.py:", e)
     traceback.print_exc()
