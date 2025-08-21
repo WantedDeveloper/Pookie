4a2662f88da165c02d1baae8b2e27a57e7deaf37
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-21 16:55:45 +0530

    Add files via upload

--- clone_plugins/start.py
+++ clone_plugins/start.py
@@ -1,4 +1,4 @@
-import logging, asyncio, base64, datetime, time, motor.motor_asyncio
+import logging, asyncio, json, base64, requests, datetime, time, motor.motor_asyncio
 from validators import domain
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
@@ -160,6 +160,74 @@ async def start(client, message):
         )
         await query.answer("❌ An error occurred. The admin has been notified.", show_alert=True)
 
+@Client.on_message(filters.command('api') & filters.private)
+async def shortener_api_handler(client, m: Message):
+    user_id = m.from_user.id
+    user = await get_user(user_id)
+    cmd = m.command
+
+    if len(cmd) == 1:
+        s = script.SHORTENER_API_MESSAGE.format(base_site=user["base_site"], shortener_api=user["shortener_api"])
+        return await m.reply(s)
+
+    elif len(cmd) == 2:    
+        api = cmd[1].strip()
+        await update_user_info(user_id, {"shortener_api": api})
+        await m.reply("Shortener API updated successfully to " + api)
+    else:
+        await m.reply("You are not authorized to use this command.")
+
+@Client.on_message(filters.command("base_site") & filters.private)
+async def base_site_handler(client, m: Message):
+    user_id = m.from_user.id
+    user = await clonedb.get_user(user_id)
+    cmd = m.command
+    text = f"/base_site (base_site)\n\nCurrent base site: None\n\n EX: /base_site shortnerdomain.com\n\nIf You Want To Remove Base Site Then Copy This And Send To Bot - `/base_site None`"
+    
+    if len(cmd) == 1:
+        return await m.reply(text=text, disable_web_page_preview=True)
+    elif len(cmd) == 2:
+        base_site = cmd[1].strip()
+        if not domain(base_site):
+            return await m.reply(text=text, disable_web_page_preview=True)
+        await clonedb.update_user_info(user_id, {"base_site": base_site})
+        await m.reply("Base Site updated successfully")
+    else:
+        await m.reply("You are not authorized to use this command.")
+
+async def get_short_link(user, link):
+    api_key = user["shortener_api"]
+    base_site = user["base_site"]
+    print(user)
+    response = requests.get(f"https://{base_site}/api?api={api_key}&url={link}")
+    data = response.json()
+    if data["status"] == "success" or rget.status_code == 200:
+        return data["shortenedUrl"]
+
+@Client.on_message(filters.command(['genlink']) & filters.user(ADMINS) & filters.private)
+async def link(client: Client, message):
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply('Reply to a message to get a shareable link.')
+    file_type = replied.media
+    if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
+        return await message.reply("Reply to a supported media")
+
+    file_id = getattr(replied, file_type.value).file_id
+    string = 'file_'
+    string += file_id
+    outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
+    user_id = message.from_user.id
+    user = await clonedb.get_user(user_id)
+    # Get the bot's username
+    bot_username = (await client.get_me()).username
+    share_link = f"https://t.me/{bot_username}?start={outstr}"
+    if user["shortener_api"]:
+        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
+    else:
+        short_link = await get_short_link(user, share_link)
+        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
+
 # Broadcast sender with error handling
 async def broadcast_messages(bot_id, user_id, message):
     try:
@@ -188,8 +256,8 @@ def make_progress_bar(done, total):
     return "🟩" * filled + "⬛" * empty
 
 # Clone broadcast command with reply-to-message and /cancel support
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS)& filters.private)
-async def pm_broadcast(bot, message):
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.private)
+async def broadcast(bot, message):
     try:
         me = await bot.get_me()
 
