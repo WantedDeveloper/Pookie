8e5048499409d568e0534da9c9be320b62de6ea3
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-24 23:19:05 +0530

    Add files via upload

--- plugins/start.py
+++ plugins/start.py
@@ -687,59 +687,6 @@ async def broadcast(bot, message):
         )
         print(f"⚠️ Broadcast Error: {e}")
 
-@Client.on_message(filters.command("contact") & filters.user(OWNERS) & filters.private)
-async def contactAdmin(bot, message):
-    try:
-        user = "@" + message.from_user.username if message.from_user.username else message.from_user.mention
-        if not message.reply_to_message:
-            return await message.reply("Please use the method described in the image to contact admin[.](https://telegra.ph/file/9a4039a2d602486cf1c00.jpg)")
-        if not message.reply_to_message.text:
-            return await message.reply("Please use the method described in the image to contact admin[.](https://telegra.ph/file/9a4039a2d602486cf1c00.jpg)")
-        await bot.send_message(
-            chat_id=OWNERS, 
-            text=f"<bold>From:</bold> {user} <bold>Id:</bold> <code>{message.chat.id}</code>\n{message.reply_to_message.text.html}"
-        )
-        userMsg = await bot.send_message(
-            chat_id=message.chat.id,
-            text="Your message has been successfully sent to Admin.",
-            reply_to_message_id=message.reply_to_message.id
-        )
-        await asyncio.sleep(5)
-        await userMsg.delete()
-    except Exception as e:
-        await client.send_message(
-            LOG_CHANNEL,
-            f"⚠️ Contact Error:\n\n<code>{e}</code>\n\nKindly check this message for assistance."
-        )
-        print(f"⚠️ Contact Error: {e}")
-
-@Client.on_message(filters.private & filters.user(OWNERS))
-async def replyUser(bot, message):
-    try:
-        if message.reply_to_message:
-            chat = int(message.reply_to_message.text.split("\n")[0][-10::])
-            try:
-                await bot.send_message(
-                    chat_id=chat,
-                    text=message.text
-                )
-                adminMsg = await message.reply(
-                    text="Successfully sent reply to User.",
-                    quote=True
-                )
-                await asyncio.sleep(5)
-                await adminMsg.delete()
-            except UserIsBlocked:
-                return await message.reply(
-                    text="User has blocked me."
-                )
-    except Exception as e:
-        await client.send_message(
-            LOG_CHANNEL,
-            f"⚠️ Reply Error:\n\n<code>{e}</code>\n\nKindly check this message for assistance."
-        )
-        print(f"⚠️ Reply Error: {e}")
-
 async def show_clone_menu(client, message, user_id):
     try:
         clones = await db.get_clones_by_user(user_id)
