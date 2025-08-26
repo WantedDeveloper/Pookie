8bdb35dfd304f0ed2bb0eb80dd5d8104fd8e2758
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-26 12:19:21 +0530

    Add files via upload

--- clone_plugins/start.py
+++ clone_plugins/start.py
@@ -113,17 +113,17 @@ async def verify_user(bot, userid, token):
     user = await bot.get_users(userid)
     TOKENS[user.id] = {token: True}
     tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
+    today = datetime.date.today()
     VERIFIED[user.id] = str(today)
 
 async def check_verification(bot, userid):
     user = await bot.get_users(userid)
     tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
+    today = datetime.date.today()
     if user.id in VERIFIED.keys():
         EXP = VERIFIED[user.id]
         years, month, day = EXP.split('-')
-        comp = date(int(years), int(month), int(day))
+        comp = datetime.date(int(years), int(month), int(day))
         if comp<today:
             return False
         else:
