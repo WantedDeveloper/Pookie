f54680cf301beb6ee863bcf77475165aeb42bf15
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-08-31 18:05:48 +0530

    Add files via upload

--- owner/owner.py
+++ owner/owner.py
@@ -2629,6 +2629,8 @@ async def message_capture(client: Client, message: Message):
                 await asyncio.sleep(2)
                 await show_post_menu(client, orig_msg, bot_id)
                 AUTO_POST.pop(user_id, None)
+                clone = await db.get_clone_by_id(bot_id)
+                print(f"📌 [DEBUG] After update_clone: {clone}")
             except Exception as e:
                 await client.send_message(LOG_CHANNEL, f"⚠️ Update Auto Post Error:\n\n<code>{e}</code>\n\nKindly check this message to get assistance.")
                 await orig_msg.edit_text(f"❌ Failed to update **auto post**: {e}")
