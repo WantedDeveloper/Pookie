b15dd7c6bef86f5da846547006861742c5adf907
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-09-04 21:40:10 +0530

    Add files via upload

--- owner/owner.py
+++ owner/owner.py
@@ -2684,6 +2684,7 @@ async def message_capture(client: Client, message: Message):
                         caption=message.caption or "",
                         media_type=media_type,
                         date=int(message.date.timestamp()),
+                        posted=False
                     )
                     print(f"✅ Saved media: {media_type} ({media_file_id})")
                 except FloodWait as e:
@@ -2694,6 +2695,7 @@ async def message_capture(client: Client, message: Message):
                         caption=message.caption or "",
                         media_type=media_type,
                         date=int(message.date.timestamp()),
+                        posted=False
                     )
                     print(f"✅ Saved media: {media_type} ({media_file_id})")
                 except Exception as e:
