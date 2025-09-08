d6d64733079d0f6edc9edb3056138097bba58c74
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-09-08 09:15:27 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -4,4 +4,8 @@ API_ID = 15479023
 API_HASH = "f8f6cf547822449c29fc60dae3b31dd4"
 
 app = Client("gen", api_id=API_ID, api_hash=API_HASH)
-print(app.export_session_string())
+
+with app:
+    print("\n✅ Your SESSION STRING:\n")
+    print(app.export_session_string())
+    print("\n⚠️ Copy this string and paste it into config.py as SESSION_STRING")
