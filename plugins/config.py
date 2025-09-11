4565b726ac769dcc720caf172c107a40c9b0c7f1
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-09-11 14:22:39 +0530

    Add files via upload

--- plugins/config.py
+++ plugins/config.py
@@ -40,10 +40,10 @@ try:
 
     # auth_channel means force subscribe channel.
     # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
-    REQUEST_TO_JOIN_MODE = bool(os.environ.get('REQUEST_TO_JOIN_MODE', True)) # Set True Or False
+    REQUEST_TO_JOIN_MODE = bool(os.environ.get('REQUEST_TO_JOIN_MODE', False)) # Set True Or False
 
     # This Is Force Subscribe Channel, also known as Auth Channel 
-    auth_channel = os.environ.get('AUTH_CHANNEL', '-1002855763957') # give your force subscribe channel id here else leave it blank
+    auth_channel = os.environ.get('AUTH_CHANNEL', '-1002912952165') # give your force subscribe channel id here else leave it blank
     AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 except Exception as e:
